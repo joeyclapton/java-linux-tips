24014d7631fc8e7dc2040a770bf2f90b6f3b1727
Joey Clapton <joeyclapton42@gmail.com>
2024-01-23 21:04:03 -0300

    feat: variables learning

---
+++ com/linuxtips/day03/Variables.java
@@ -0,0 +1,31 @@
+package com.linuxtips.day03;
+
+public class Variables {
+  public static void main(String[] args) {
+
+    byte network = 127;
+    System.out.println("Byte: " + network);
+
+    short houseNumber = 323;
+    System.out.println("Short: " + houseNumber);
+
+    int age = 25;
+    System.out.println("Int: " + age);
+
+    long ip = 192168001;
+    System.out.println("Long: " + ip);
+
+    double salary = 102_51;
+    System.out.println("Double: " + salary);
+
+    float cm = 178.56F;
+    System.out.println("Float: " + cm);
+
+    char choice = 'B';
+    System.out.println("Char: " + choice);
+
+    boolean isSaved = false;
+    System.out.println("Boolean: " + isSaved);
+
+  }
+}
