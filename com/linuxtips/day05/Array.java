15d78feb9af6bb96e796bcf7377edaa11a14b2c0
Joey Clapton <joeyclapton42@gmail.com>
2024-01-23 23:01:29 -0300

    feat: add new arrays methods

--- com/linuxtips/day05/Array.java
+++ com/linuxtips/day05/Array.java
@@ -30,7 +30,34 @@ public class Array {
 
       String[] onlyCourseNameCopy = Arrays.copyOf(courseName, 1);
       List.of(onlyCourseNameCopy).forEach(System.out::println);
+      
+      System.out.println("-----------------------------------------------");
+      // Part 2 Arrays
+      arrayOtherMethods();
+    }
 
+    public static void arrayOtherMethods() {
+      Integer[] total = new Integer[10];
+      Arrays.fill(total, 40);
+
+      Arrays.asList(total).forEach(System.out::println);
+
+      System.out.println("------------------------------------");
+
+      String[] mathClass = new String[]{"Sarah", "Jonh Doe", "Ana"};
+      String[] englishClass = new String[]{"John Doe", "Ana", "Sarah"};
+      String[] frenchClass = Arrays.copyOf(mathClass, mathClass.length);
+
+      System.out.println("As turmas de math/english tem alunos iguais? " + Arrays.equals(mathClass, englishClass));
+      System.out.println("As turmas de math/french tem alunos iguais? " + Arrays.equals(mathClass, frenchClass));
+
+      System.out.println("------------------------------------");
+      Integer[] list = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+      Integer[] copy = Arrays.copyOf(list, list.length);
+
+      Arrays.sort(copy);
+
+      Arrays.asList(copy).forEach(System.out::println);
     }
   
 }
