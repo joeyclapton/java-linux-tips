596c132c0c281dfd810f926eafcdebe2d5f47f6e
Joey Clapton <joeyclapton42@gmail.com>
2024-01-24 17:45:19 -0300

    feat: add arrays hands on

--- com/linuxtips/day05/HandsOnArray.java
+++ com/linuxtips/day05/HandsOnArray.java
@@ -19,7 +19,23 @@ public class HandsOnArray {
     System.out.println("Idade de metade da turma ✨");
     List.of(halfAges).forEach(System.out::println);
 
+    System.out.println("---------------------------------------");
 
+    String[] names = new String[8];
+    Arrays.fill(names, "James");
+
+    List<String> namesList = List.of(names);
+    namesList.forEach(System.out::println);
+
+    System.out.println("---------------------------------------");
+    String[] englishClass = new String[]{"João", "Ana", "César"};
+    String[] mathClass = new String[]{"João", "Ana", "César"};
+
+    // Comparando referência dos objetos
+    System.out.println(englishClass.equals(mathClass));
+
+    // Comparando o conteúdo dentro dos arrays
+    System.out.println(Arrays.equals(englishClass, mathClass));
 
   }
 }
