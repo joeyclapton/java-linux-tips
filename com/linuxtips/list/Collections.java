b1d8d286f9868c236ad39f241a40ebaa86c60ffb
Joey Clapton <joeyclapton42@gmail.com>
2024-03-15 17:59:06 -0300

    feat: create hashmap collection

---
+++ com/linuxtips/list/Collections.java
@@ -0,0 +1,20 @@
+import java.util.Map;
+import java.util.HashMap;
+
+public class Collections {
+    public static void main(String[] args) {
+        Map<String, Integer> todo = new HashMap<>();
+        
+        todo.put("shopping", 4);
+        todo.put("running", 3);
+
+        var running = todo.get("running");
+
+        for(Map.Entry<String, Integer> entry : todo.entrySet()) {
+            String key = entry.getKey();
+            Integer value = entry.getValue();
+            System.out.println("Key: " + key);
+            System.out.println("Value: " + value);
+        } 
+    }
+}
