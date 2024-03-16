c1b147b9366a193d7556123566071c1d2e2f4301
Joey Clapton <joeyclapton42@gmail.com>
2024-03-15 22:31:08 -0300

    feat: init spring-boot project

---
+++ hello-world-spring/src/test/java/com/app/helloworldspring/HelloWorldSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.helloworldspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
