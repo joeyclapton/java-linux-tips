1391c04f021bc4030721523e45a54780b6834570
Joey Clapton <joeyclapton42@gmail.com>
2024-03-15 23:18:30 -0300

    feat: add new endpoint informations

--- hello-world-spring/src/main/java/com/app/helloworldspring/controller/UserController.java
+++ hello-world-spring/src/main/java/com/app/helloworldspring/controller/UserController.java
@@ -1,16 +1,27 @@
 package com.app.helloworldspring.controller;
 
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
     
+    @GetMapping("/id/{id}")
+    public String showId(@PathVariable String id) {
+        var idLabel = "{" + id + "}";
+        return "O parâmetro do usuário é " + idLabel;
+    }
+
     @GetMapping("")
-    public String getInformations() {
-        return "Name: Joey Clapton, Age: 24";
+    public String showInformations(@RequestParam Map<String, String> params) {
+        return "Bom dia %s".formatted(params.entrySet());
     }
 
 }
